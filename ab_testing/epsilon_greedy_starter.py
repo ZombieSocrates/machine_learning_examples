effaa97a79273bf6ef2d9ee11da77fe23b572935
Ross Weijer <ross@datascopeanalytics.com>
2022-01-07 10:53:38 -0600

    Epsilon Greedy exercise finished in epsilon_greedy_starter.py

--- ab_testing/epsilon_greedy_starter.py
+++ ab_testing/epsilon_greedy_starter.py
@@ -58,7 +58,7 @@ class EpsilonGreedyExperiment(object):
 
 
     def progress_update(self):
-        for i, m in self.machines:
+        for i, m in enumerate(self.machines):
             print(f"Machine {i + 1}")
             m.print_summary(show_true_rate = False)
             print("------"*5, "\n")
@@ -75,29 +75,58 @@ class EpsilonGreedyExperiment(object):
 
 
     def run_experiment(self, max_trials: float, update_every: int):
-        while self.N_trials <= max_trials:
+        while self.N_trials < max_trials:
             self.run_trial()
             if self.N_trials % update_every == 0:
-                self.progress_update() 
+                self.progress_update()
+        print("Experiment Complete! Saving results and clearing data.") 
         self.plot_experiment_results()
         self.reset_experiment()
+
+
+    def plot_experiment_results(self, lbl_pad = 10):
+        plays_per_machine = [x.N_plays for x in self.machines]
+        win_rates = [x.win_rate for x in self.machines]
+        coords = [i + 1 for i in range(len(win_rates))]
+        fig, ax = plt.subplots(figsize = (10, 10))
+        plt.bar(x = coords, height = plays_per_machine)
+        ax.set_xticks(coords)
+        ax.set_xticklabels([f"Machine {i}" for i in coords], rotation = 65,
+            ha = "right")
+        ax.set_ylabel("Number of Plays")
+        for idx, wr in enumerate(win_rates):
+            ax.text(x = coords[idx], y = plays_per_machine[idx] + lbl_pad, 
+                s = f"{wr:0.4%}", ha = "center")
+        total_trials = sum(plays_per_machine)
+        plt.title(f"Epsilon Greedy: {total_trials} Trials; {self.eps:0.2%} Base Epsilon")
+        plt.tight_layout()
+        save_path = str(self.get_experiment_save_path())
+        print(save_path)
+        plt.savefig(save_path)
+
+
+    def get_experiment_save_path(self, base_dir = "viz/epsilon_greedy", ext = ".png"):
+        base_pth = pathlib.Path(base_dir)
+        if not base_pth.exists():
+            base_pth.mkdir(exist_ok = True, parents = True)
+        exp_index = len([k for k in base_pth.glob(f"*{ext}")])
+        next_index = str(exp_index + 1).rjust(2, "0")
+        exp_file = base_pth / f"experiment_{next_index}_results{ext}"
+        return exp_file
 
 
     def reset_experiment(self):
         self.N_trials = 0 
         for machine in self.machines:
             machine.N_plays = 0 
-            machinee.win_rate = None
-
-
-
-
-
-
+            machine.win_rate = None
 
 
 if __name__ == "__main__":
 
     probas = [0.2, 0.5, 0.8]
     epsilon = 0.05
-    
+    max_trials = 10000
+    epg_lab = EpsilonGreedyExperiment(machine_win_rates = probas, eps = epsilon)
+    epg_lab.run_experiment(max_trials = max_trials, update_every = 1000)
+
