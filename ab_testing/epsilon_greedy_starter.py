f69c7e77cac86119c0d14b317b6cb3362eb98d43
Ross Weijer <ross@datascopeanalytics.com>
2022-01-07 09:51:15 -0600

    WIP epsilon greedy experiment....gitignore a visuals folder for ab_testing course

--- ab_testing/epsilon_greedy_starter.py
+++ ab_testing/epsilon_greedy_starter.py
@@ -1,88 +1,103 @@
-# From the course: Bayesin Machine Learning in Python: A/B Testing
-# https://deeplearningcourses.com/c/bayesian-machine-learning-in-python-ab-testing
-# https://www.udemy.com/bayesian-machine-learning-in-python-ab-testing
-from __future__ import print_function, division
-from builtins import range
-# Note: you may need to update your version of future
-# sudo pip install -U future
+import pathlib 
+import numpy as np   
+
+from matplotlib import pyplot as plt
+from typing import List
+
+class SlotMachine(object):
 
 
-import matplotlib.pyplot as plt
-import numpy as np
+    def __init__(self, true_win_rate: float):
+        self.true_win_rate = true_win_rate
+        self.N_plays = 0 
+        self.win_rate = None  
 
 
-NUM_TRIALS = 10000
-EPS = 0.1
-BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
+    def pull_arm(self):
+        pull_val = np.random.uniform(size = 1)[0]
+        return 1 if pull_val <= self.true_win_rate else 0 
+  
+
+    def update_win_rate(self, value):
+        self.N_plays += 1 
+        prior = 0 if self.win_rate is None else self.win_rate
+        updated =  1 / self.N_plays * (value + (self.N_plays - 1) * prior)
+        self.win_rate = updated
 
 
-class BanditArm:
-  def __init__(self, p):
-    # p: the win rate
-    self.p = p
-    self.p_estimate = # TODO
-    self.N = # TODO
-
-  def pull(self):
-    # draw a 1 with probability p
-    return np.random.random() < self.p
-
-  def update(self, x):
-    self.N = # TODO
-    self.p_estimate = # TODO
+    def play(self):
+        pull_val = self.pull_arm()
+        self.update_win_rate(value = pull_val)
 
 
-def experiment():
-  bandits = [BanditArm(p) for p in BANDIT_PROBABILITIES]
+    def print_summary(self, show_true_rate = False):
+        print(f"\tTimes played: {self.N_plays}")
+        if self.win_rate is not None:
+            print(f"\tCurrent Win Rate: {self.win_rate:0.4%}")
+        if show_true_rate:
+            print(f"\tTrue Win Rate: {self.true_win_rate:0.2%}")
 
-  rewards = np.zeros(NUM_TRIALS)
-  num_times_explored = 0
-  num_times_exploited = 0
-  num_optimal = 0
-  optimal_j = np.argmax([b.p for b in bandits])
-  print("optimal j:", optimal_j)
 
-  for i in range(NUM_TRIALS):
+class EpsilonGreedyExperiment(object):
 
-    # use epsilon-greedy to select the next bandit
-    if np.random.random() < EPS:
-      num_times_explored += 1
-      j = # TODO
-    else:
-      num_times_exploited += 1
-      j = # TODO
 
-    if j == optimal_j:
-      num_optimal += 1
+    def __init__(self, machine_win_rates: List[float], eps: float):
+        self.eps = eps 
+        self.machines = [SlotMachine(true_win_rate = p) for p in machine_win_rates]
+        self.N_trials = 0
 
-    # pull the arm for the bandit with the largest sample
-    x = bandits[j].pull()
 
-    # update rewards log
-    rewards[i] = x
+    def choose_random_machine(self):
+        return np.random.choice(self.machines)
 
-    # update the distribution for the bandit whose arm we just pulled
-    bandits[j].update(x)
 
-    
+    def choose_best_machine(self):
+        win_sort = lambda x: x.win_rate if x.win_rate is not None else -1
+        sorted_machines = sorted(self.machines, key = win_sort)
+        return sorted_machines[-1]
 
-  # print mean estimates for each bandit
-  for b in bandits:
-    print("mean estimate:", b.p_estimate)
 
-  # print total reward
-  print("total reward earned:", rewards.sum())
-  print("overall win rate:", rewards.sum() / NUM_TRIALS)
-  print("num_times_explored:", num_times_explored)
-  print("num_times_exploited:", num_times_exploited)
-  print("num times selected optimal bandit:", num_optimal)
+    def progress_update(self):
+        for i, m in self.machines:
+            print(f"Machine {i + 1}")
+            m.print_summary(show_true_rate = False)
+            print("------"*5, "\n")
 
-  # plot the results
-  cumulative_rewards = np.cumsum(rewards)
-  win_rates = cumulative_rewards / (np.arange(NUM_TRIALS) + 1)
-  plt.plot(win_rates)
-  plt.plot(np.ones(NUM_TRIALS)*np.max(BANDIT_PROBABILITIES))
-  plt.show()
+
+    def run_trial(self):
+        eps_draw = np.random.uniform(size = 1)[0]
+        if self.N_trials == 0 or eps_draw <= self.eps:
+            machine = self.choose_random_machine()
+        else:
+            machine = self.choose_best_machine()
+        machine.play()
+        self.N_trials += 1
+
+
+    def run_experiment(self, max_trials: float, update_every: int):
+        while self.N_trials <= max_trials:
+            self.run_trial()
+            if self.N_trials % update_every == 0:
+                self.progress_update() 
+        self.plot_experiment_results()
+        self.reset_experiment()
+
+
+    def reset_experiment(self):
+        self.N_trials = 0 
+        for machine in self.machines:
+            machine.N_plays = 0 
+            machinee.win_rate = None
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
-  experiment()
+
+    probas = [0.2, 0.5, 0.8]
+    epsilon = 0.05
+    
